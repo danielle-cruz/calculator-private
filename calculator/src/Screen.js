6320a506453599c2230660c81f8a86060df8a7c3
Danielle Cruz <dcruz21@stanford.edu>
2021-03-14 01:07:42 -0600

    Renamed to calculator working

---
+++ calculator/src/Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+class Screen extends React.Component {
+
+  render() {
+    return (
+      <div className="screen">
+        The screen goes here
+      </div>
+    );
+  }
+}
+
+export default Screen;
--- calculator/src/index.js
+++ calculator/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Screen from './Screen';
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Screen />
   </React.StrictMode>,
   document.getElementById('root')
 );
