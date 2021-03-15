38bbdf6275f0070c758ba0c1d04c74c8781ce360
Danielle Cruz <dcruz21@stanford.edu>
2021-03-15 01:56:28 -0500

    Added comments to evaluate.js and styles.css

--- calculator/src/evaluate.js
+++ calculator/src/evaluate.js
@@ -1,3 +1,57 @@
+/*** Button Actions ***/
+
+/* PEMDAS Evaluate: Evaluate the current expression according to PEMDAS ordering,
+ * solving exponents, then multiplcation and division (left to right), and then
+ * addition and subtraction (left to right). */
+export function pemdasEvaluate(expression) {
+  // Create an array of the numbers and operands of the expression
+  let elements = expression.split(' ');
+  elements = solveExponents(elements);
+  elements = solveMultDiv(elements);
+  elements = solveAddSub(elements);
+
+  // If the result is NaN, return an error
+  if (isNaN(elements[0])) {
+    alert('Error: Invalid input.')
+    return 'Error';
+  }
+  return elements[0];
+
+}
+
+/* Backspace: Backspaces numbers and operands from the expression one at a time.
+ * Updates appropriate states to reflect these changes. */
+export function backspace(expression) {
+  let elements = expression.split(' ');
+  let deleted = elements.pop();
+  if (deleted === '') {
+    elements.pop();
+  }
+  console.log(elements);
+
+  // Re-form the current expression without the backspaced element
+  let newExpression = '';
+  for (let element of elements) {
+    newExpression += element + ' ';
+  }
+
+  // Keep track of the type that now appears last in the expression in order
+  //  to inform what kinds of characters can be typed to create a valid exprssion
+  let lastType;
+  if (elements.length === 0) {
+    lastType = null;
+  } else if (elements.length % 2 === 0) {
+    lastType = 'operation';
+  } else {
+    lastType = 'digits';
+    newExpression = newExpression.substring(0, newExpression.length-1);
+  }
+
+  return [lastType, newExpression];
+
+}
+
+/*** Helper Functions ***/
 function solveExponents(elements) {
   for (let i = 1; i < elements.length; i++) {
     if (elements[i] === '^') {
@@ -37,46 +91,4 @@ function solveAddSub(elements) {
     }
   }
   return elements;
-}
-
-/* === Button functions === */
-
-export function pemdasEvaluate(expression) {
-  let elements = expression.split(' ');
-  elements = solveExponents(elements);
-  elements = solveMultDiv(elements);
-  elements = solveAddSub(elements);
-  if (isNaN(elements[0])) {
-    alert('Error: Invalid input.')
-    return 'Error';
-  }
-  return elements[0];
-
-}
-
-export function backspace(expression) {
-  let elements = expression.split(' ');
-  let deleted = elements.pop();
-  if (deleted === '') {
-    elements.pop();
-  }
-  console.log(elements);
-
-  let newExpression = '';
-  for (let element of elements) {
-    newExpression += element + ' ';
-  }
-
-  let lastType;
-  if (elements.length === 0) {
-    lastType = null;
-  } else if (elements.length % 2 === 0) {
-    lastType = 'operation';
-  } else {
-    lastType = 'digits';
-    newExpression = newExpression.substring(0, newExpression.length-1);
-  }
-
-  return [lastType, newExpression];
-
 }
